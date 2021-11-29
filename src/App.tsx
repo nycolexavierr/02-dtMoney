c6ebc5e479f878f450476de3f15667e7bcd36797
Nycole Xavier <nycole.xavier@hotmail.com>
2021-11-29 07:38:31 -0400

    lesson: Instalando Styled Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  font-size: 64px;
+  color: #8257e6;
+`
+
 export function App() {
   return (
     <div className="App">
-      <h1>Hellow World</h1>
+      <Title>Hello World</Title>
     </div>
   );
 }
