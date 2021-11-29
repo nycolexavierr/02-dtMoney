d88cc755232fde4f757d8cc3aefbc4f60dbd7b59
Nycole Xavier <nycole.xavier@hotmail.com>
2021-11-29 15:31:28 -0400

    Lesson: Componente: Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <>
+      <Header/>
       <GlobalStyle/>
-    </div>
+    </>
   );
 }
 
