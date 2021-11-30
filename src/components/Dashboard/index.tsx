ccc4d11cac50867008383f3f4095c1cb1eefebfe
Nycole Xavier <nycole.xavier@hotmail.com>
2021-11-30 08:35:48 -0400

    Lesson: ---

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Summary } from "../Summary/Index";
+import { TransactionTable } from "../TransactionTable";
 import { Container } from "./styles";
 
 export function Dashboard () {
     return (
         <Container>
             <Summary/>
+            <TransactionTable/>
         </Container>
     );
 }
---
+++ src/components/TransactionTable/index.tsx
@@ -0,0 +1,42 @@
+import { Container } from "./styles";
+
+export function TransactionTable() {
+    return(
+        <Container>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Título</th>
+                        <th>Valor</th>
+                        <th>Categoria</th>
+                        <th>Data</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>Desenvolvimento de Website</td>
+                        <td>R$ 12.000</td>
+                        <td>Desenvolvimento</td>
+                        <td>20/02/2021</td>
+                    </tr>
+                    <tr>
+                        <td>Desenvolvimento de Website</td>
+                        <td>R$ 12.000</td>
+                        <td>Desenvolvimento</td>
+                        <td>20/02/2021</td>
+                    </tr>
+                    <tr>
+                        <td>Desenvolvimento de Website</td>
+                        <td>R$ 12.000</td>
+                        <td>Desenvolvimento</td>
+                        <td>20/02/2021</td>
+                    </tr>
+                    
+                </tbody>
+
+            </table>
+
+        </Container>
+    )
+}
