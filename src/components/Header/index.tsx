acddb1b467fb0a58cc555ccafbd1ec7d2bf595b2
Nycole Xavier <nycole.xavier@hotmail.com>
2021-12-01 08:52:30 -0400

    Lesson: Configurando modal de criação

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,21 @@
+import { useState } from 'react'
+import Modal from 'react-modal'
 import logoImg from '../../assets/logo.svg'
 import { Container, Content } from './styles'
 
-export function Header() {
+
+interface ReaderProps {
+    onOpenNewTransactionModal: () => void;
+}
+
+export function Header({onOpenNewTransactionModal}: ReaderProps) {
+
+
     return(
         <Container>
             <Content>
                 <img src={logoImg} alt="dt money" />
-                <button type="button" >
+                <button type="button" onClick={onOpenNewTransactionModal}>
                     Nova transação
                 </button>
             </Content>
