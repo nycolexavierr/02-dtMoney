05dd309eff93ac6e41aea96330b41ed81825220a
Nycole Xavier <nycole.xavier@hotmail.com>
2021-12-01 13:31:35 -0400

    Estilizando modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,5 @@
-import { VoidFunctionComponent } from 'react';
 import Modal from 'react-modal';
+import closeImg from '../../assets/close.svg';
 import { Container } from './styles';
 
 
@@ -16,6 +16,9 @@ export function NewTransactionModal ({isOpen, onRequestClose} : NewTransactionMo
             overlayClassName="react-modal-overlay"
             className="react-modal-content"
         >
+            <button type="button" onClick={onRequestClose} className="react-modal-close" >
+                <img src={closeImg} alt="Fechar modal" />
+            </button>
 
             <Container>
                 <h2>Cadastrar transação</h2>
