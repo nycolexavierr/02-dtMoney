0cbf18af3cf52826bd22aa066bdd69969f8fecc0
Nycole Xavier <nycole.xavier@hotmail.com>
2021-12-01 18:16:48 -0400

    Lesson: Inserindo transação na API

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import {App} from './App';
+import Schema from 'miragejs/orm/schema';
 
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+
   routes() {
     this.namespace = 'api';
 
     this.get('/transactions', () => {
-      return [
-        {
-            id: 1,
-            title: 'Transaction 1',
-            amount: 400,
-            type: 'deposit',
-            category: 'Food',
-        }
-      ]
+      return this.schema.all('transaction')
+    })
+
+    this.post('/transactions', (schema, request ) => {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data)
     })
   } 
 })
