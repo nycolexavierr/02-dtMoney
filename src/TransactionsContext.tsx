846d614230cba267207331f5b749dc08d8837975
Nycole Xavier <nycole.xavier@hotmail.com>
2021-12-03 07:44:57 -0400

    Lesson: Finalizando inserção

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -18,7 +18,7 @@ interface TransactionsProviderPosps {
 
 interface TransactionsContextData {
     transactions: Transaction[];
-    createTransaction: (transaction: TransactionInput) => void;
+    createTransaction: (transaction: TransactionInput) => Promise<void>;
 }
 
 export const TransactionsContext = createContext<TransactionsContextData>(
@@ -33,8 +33,17 @@ export function TransactionsProvider({ children }: TransactionsProviderPosps) {
         .then(response => setTransactions(response.data.transactions))
     }, []);
 
-    function createTransaction(transaction: TransactionInput) {
-        api.post('/transactions', transactions)
+    async function createTransaction(transactionInput: TransactionInput) {
+        const response = await api.post('/transactions', {
+            ...transactionInput,
+            createdAt: new Date()
+        })
+        const {transaction} = response.data;
+
+        setTransactions([
+            ...transactions,
+            transaction,
+        ])
     }
 
     return(
